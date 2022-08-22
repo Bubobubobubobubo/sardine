2b8afd65604f2f6b2e97d96fbdd3ecd4143b8dc4
Raphael Forment <raphael.forment@gmail.com>
2022-08-22 17:24:54 +0200

    Adding domain name

--- sardine/__init__.py
+++ sardine/__init__.py
@@ -74,9 +74,9 @@ stop = c.remove
 children = c.print_children
 S = c.note  # default SuperDirt
 M = c.midinote  # default Midi Connexion
-O = c.oscmessage # default OSC Sender
+O = c.oscmessage  # default OSC Sender
 MidiSend = MIDISender
-# O = OSCSender
+# O = OSCSender
 n = next
 
 

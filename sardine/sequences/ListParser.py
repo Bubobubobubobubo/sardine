1ee97be5e26b890da147b5f0f338b05dbf35f5d1
Raphael Forment <raphael.forment@gmail.com>
2022-08-24 00:48:00 +0200

    Modified note grammar to include more rules: - chords can be written manually by grouping between parenthesis (). - notes can be raised (+) or lowered manually by an octave
    I don't like the chord syntax at all. It causes all sorts of problems.

--- sardine/sequences/ListParser.py
+++ sardine/sequences/ListParser.py
@@ -8,14 +8,38 @@ __all__ = ("ListParser", "Pnote", "Pname", "Pnum")
 class ParsingError(Exception):
     pass
 
+# I don't know if it is worth it to go further. It's probably
+# better to stack chords at this point to obtain the desired
+# quality.
 qualifiers = {
-    'maj' : [0, 4, 7],
-    'min' : [0, 3, 7],
-    'sus4': [0, 5, 7],
-    'sus2': [0, 2, 7],
-    'b5': [0, 4, 6],
-    'mb5': [0, 3, 6],
+    'dim'    : [0, 3, 6, 12],
+    'dim9'   : [0, 3, 6, 9, 14],
+    'hdim7'  : [0, 3, 6, 10],
+    'hdim9'  : [0, 3, 6, 10, 14],
+    'hdimb9'  : [0, 3, 6, 10, 13],
+    'dim7'   : [0, 3, 6, 9],
+    '7dim5'  : [0, 4, 6, 10],
+    'aug'    : [0, 4, 8, 12],
+    'augMaj7': [0, 4, 8, 11],
+    'aug7'   : [0, 4, 8, 10],
+    'aug9'   : [0, 4, 10, 14],
+    'maj'    : [0, 4, 7, 12],
+    'maj7'   : [0, 4, 7, 11],
+    'maj9'   : [0, 4, 11, 14],
+    'minmaj7': [0, 3, 7, 11],
+    '7'      : [0, 4, 7, 10],
+    '9'      : [0, 4, 10, 14],
+    'b9'     : [0, 4, 10, 13],
+    'mM9'    : [0, 3, 11, 14],
+    'min'    : [0, 3, 7, 12],
+    'min7'   : [0, 3, 7, 10],
+    'min9'   : [0, 3, 10, 14],
+    'sus4'   : [0, 5, 7, 12],
+    'sus2'   : [0, 2, 7, 12],
+    'b5'     : [0, 4, 6, 12],
+    'mb5'    : [0, 3, 6, 12],
 }
+
 
 number_grammar = """
 
@@ -67,19 +91,24 @@ note_grammar = """
     ?number: /[0-9]/
 
     ?note: atom
-         | atom "|" atom                 -> choice_note
-         | atom "!" number               -> repeat_note
-         | atom ":" NAME                 -> add_qualifier
+         | atom "/" atom                     -> slash_chord
+         | atom "|" atom                     -> choice_note
+         | atom "!" number                   -> repeat_note
+         | atom ":" NAME                     -> add_qualifier
+         | atom "_" number                   -> invert_chord
 
-    pure_atom: /[a-gA-G]/                    -> make_note_anglo_saxon
+    pure_atom: /[A-G]/                       -> make_note_anglo_saxon
              | /do|re|ré|mi|fa|sol|la|si/    -> make_note_french_system
     
     ?atom: pure_atom
-         | atom "0".."9"                 -> add_octave
-         | atom "#"                      -> sharp_simple
-         | atom "b"                      -> flat_simple
-         | atom "#" number               -> sharp_octave
-         | atom "b" number               -> flat_octave
+         | atom "0".."9"                     -> add_octave
+         | atom "#"                          -> sharp_simple
+         | atom "b"                          -> flat_simple
+         | atom "-"                          -> drop_octave
+         | atom "+"                          -> raise_octave
+         | atom "#" number                   -> sharp_octave
+         | atom "b" number                   -> flat_octave
+         | "(" atom (atom)* ")"
     
     %import common.CNAME -> NAME
 """
@@ -97,9 +126,11 @@ class CalculateTree(Transformer):
 
     # Notes
     def make_note_anglo_saxon(self, symbol):
-        table = {'c':0, 'd':2, 'e':4, 'f':5, 
-                'g':7, 'a':9, 'b':11}
+        table = {'C':0, 'D':2, 'E':4, 'F':5, 
+                'G':7, 'A':9, 'B':11}
         return table[symbol]
+    def make_notes(self, symbols):
+        return list(symbols)
     def make_note_french_system(self, symbol):
         table = {'do': 0, 're': 2, 'ré': 2, 'mi':4, 'fa':5, 'sol':7, 'la':9, 'si':11}
         return table[symbol]
@@ -111,9 +142,16 @@ class CalculateTree(Transformer):
     def flat_octave(self, note, number): return note+12*int(number)-1
     def choice_note(self, note0, note1): return random.choice([note0, note1])
     def repeat_note(self, note, number): return [note]*int(number)
+    def drop_octave(self, note): return note - 12
+    def raise_octave(self, note): return note + 12
     def add_qualifier(self, note, qualifier):
         return [note+x for x in qualifiers[qualifier]]
-        
+    def invert_chord(self, notes):
+        pass
+    def slash_chord(self, note0, note1):
+        note0, note1 = [note0], [note1]
+        return note0.extend(note1)
+
 
     def id(self, a):
         return a

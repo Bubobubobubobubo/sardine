999fd767843c39308c60acb38e27e338d8af0439
mugulmd <mugulmotion@gmail.com>
2022-11-03 20:14:01 +0100

    [tests] using sub-tests for handling multiple patterns

--- tests/test_pattern_parsing.py
+++ tests/test_pattern_parsing.py
@@ -5,9 +5,25 @@ from sardine.sequences.LexerParser.ListParser import ListParser
 class TestPatternParsing(unittest.TestCase):
 
     def test_number_pattern(self):
+        """
+        Test parsing several patterns composed of numbers and simple math operations.
+        """
         parser = ListParser(None, None, None)
-        pattern = "1, 2, 3"
-        result = parser.parse(pattern)
-        expected = [1, 2, 3]
-        self.assertEqual(len(result), len(expected))
-        self.assertTrue(all([x == y for x, y in zip(result, expected)]))
+        patterns = [
+            ".5",
+            "1, 2, 3",
+            "1+1, 2*3, 4-1, 5/2"
+        ]
+        expected = [
+            [.5],
+            [1, 2, 3],
+            [2, 6, 3, 2.5]
+        ]
+        for i, pattern in enumerate(patterns):
+            with self.subTest(i=i, pattern=pattern):
+                result = parser.parse(pattern)
+                self.assertEqual(len(result), len(expected[i]))
+                for x, y in zip(result, expected[i]):
+                    # we use assertAlmostEqual instead of assertEqual here
+                    # because we are dealing with floats
+                    self.assertAlmostEqual(x, y)

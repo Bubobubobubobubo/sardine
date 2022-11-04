7a4ff1fa8f2cc90605bf4cf6d733b27db7b7eaf0
Raphael Forment <raphael.forment@gmail.com>
2022-11-04 15:24:49 +0100

    adding simple note test

--- tests/test_pattern_parsing.py
+++ tests/test_pattern_parsing.py
@@ -27,3 +27,32 @@ class TestPatternParsing(unittest.TestCase):
                     # we use assertAlmostEqual instead of assertEqual here
                     # because we are dealing with floats
                     self.assertAlmostEqual(x, y)
+
+    def test_notes(self):
+        """
+        Test parsing a random number generator
+        """
+        parser = ListParser(None, None, None)
+        patterns = [
+                "C,D,E,F,G,A,B",
+                "Do,Re,Mi,Fa,Sol,La,Si",
+                "Do,Ré,Mi,Fa,Sol,La,Si",
+                "C0,C1,C2,C3,C4,C5,C6,C7,C8,C9",
+        ]
+        expected = [
+            [60, 62, 64, 65, 67, 69, 71],
+            [60, 62, 64, 65, 67, 69, 71],
+            [60, 62, 64, 65, 67, 69, 71],
+            [12, 24, 36, 48, 60, 72, 84, 96, 108, 120],
+        ]
+        for i, pattern in enumerate(patterns):
+            with self.subTest(i=i, pattern=pattern):
+                result = parser.parse(pattern)
+                self.assertEqual(len(result), len(expected[i]))
+                for x, y in zip(result, expected[i]):
+                    # we use assertAlmostEqual instead of assertEqual here
+                    # because we are dealing with floats
+                    self.assertEqual(x, y)
+
+if __name__ == '__main__':
+    unittest.main()

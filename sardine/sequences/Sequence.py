fb48ee8ac020b843dcda50dc61e994879371dad0
Raphael Forment <raphael.forment@gmail.com>
2022-11-09 02:02:10 +0100

    add additional fix for euclid

--- sardine/sequences/Sequence.py
+++ sardine/sequences/Sequence.py
@@ -66,6 +66,8 @@ def _starts_descent(list, index):
 
 def euclidian_rhythm(pulses: int, length: int, rotate: int= 0):
     """Calculate Euclidean rhythms"""
+    if pulses >= length:
+        return [1]
     res_list = [pulses * t % length for t in range(-1, length - 1)]
     bool_list = [_starts_descent(res_list, index) for index in range(length)]
     def rotation(l, n):

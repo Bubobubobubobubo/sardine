2d38ff740b15eefd5556276533db260c49e4ac67
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2022-11-17 07:36:50 -0500

    Initial stages of clock handler

--- sardine/base/clock.py
+++ sardine/base/clock.py
@@ -1,40 +1,59 @@
-from abc import (
-    ABC, 
-    abstractmethod)
-from typing import TYPE_CHECKING
+from abc import ABC, abstractmethod
+import contextlib
+import contextvars
+from typing import TYPE_CHECKING, Optional
+
+from .handler import BaseHandler
 
 if TYPE_CHECKING:
     from ..fish_bowl import FishBowl
 
 __all__ = ("BaseClock",)
 
+time_shift = contextvars.ContextVar("time_shift", default=0.0)
+"""
+This specifies the amount of time to offset in the current context.
+Usually this is updated within the context of scheduled functions
+to simulate sleeping without actually blocking the function. Behavior is
+undefined if time is shifted in the global context.
+"""
+
 
 # TODO: document BaseClock and its methods
-class BaseClock(ABC):
+class BaseClock(BaseHandler, ABC):
+    """The base for all clocks to inherit from.
 
+    This interface expects clocks to manage its own source of time,
+    and provide the `phase`, `beat`, and `tempo` properties.
     """
-    Template for all clocks
-    """
 
-    env: 'FishBowl'
+    env: "FishBowl"
 
+    # Abstract methods
+
+    @property
     @abstractmethod
-    def time(self):
-        """Get current time"""
+    def internal_time(self) -> Optional[float]:
+        """Returns the clock's internal time if available.
+
+        At the start of the `run()` method, this should be set as early
+        as possible in order for the `time` property to compute the
+        elapsed time.
+        """
+
+    @property
+    @abstractmethod
+    def internal_origin(self) -> Optional[float]:
+        """Returns the clock's internal time origin if available.
+
+        At the start of the `run()` method, this should be set as early
+        as possible in order for the `time` property to compute the
+        elapsed time.
+        """
 
     @abstractmethod
     def run(self):
         """Starts the clock, updating the environment's clock state."""
-
-    @property
-    @abstractmethod
-    def time_shift(self):
-        pass
-
-    @time_shift.setter
-    @abstractmethod
-    def time_shift(self, value):
-        pass
 
     @property
     @abstractmethod
@@ -51,31 +70,55 @@ class BaseClock(ABC):
     def tempo(self):
         pass
 
+    # Properties
+
     @property
-    @abstractmethod
-    def bpm(self):
-        pass
+    def time(self) -> float:
+        """Returns the current time of the fish bowl.
 
-    @abstractmethod
-    def pause(self):
-        pass
+        This uses the `internal_time` and `internal_origin` properties
+        along with the fish bowl's `Time.origin` to calculate a monotonic
+        time for the entire system.
 
-    @abstractmethod
-    def resume(self):
-        pass
+        This number will also include any time shift that has been set.
+        """
+        time, origin = self.internal_time, self.internal_origin
+        if time is None or origin is None:
+            return self.env.time.origin
+        return time - origin + self.env.time.origin + self.time_shift
 
-    @abstractmethod
-    def start(self):
-        pass
+    @property
+    def time_shift(self) -> float:
+        """The time shift in the current context.
 
-    @abstractmethod
-    def stop(self):
-        pass
+        This is useful for simulating sleeps without blocking.
+        """
+        return time_shift.get()
 
-    @abstractmethod
-    def is_running(self):
-        pass
+    @time_shift.setter
+    def time_shift(self, seconds: int):
+        time_shift.set(seconds)
 
-    @abstractmethod
-    def is_paused(self):
-        pass
+    @contextlib.contextmanager
+    def scoped_time_shift(self, seconds: float):
+        """Returns a context manager that adds `seconds` to the clock.
+
+        After the context manager is exited, the time shift is restored
+        to its previous value.
+        """
+        token = time_shift.set(time_shift.get() + seconds)
+        try:
+            yield
+        finally:
+            time_shift.reset(token)
+
+    # Handler hooks
+
+    def setup(self):
+        pass  # TODO BaseClock setup()
+
+    def teardown(self):
+        pass  # TODO BaseClock teardown()
+
+    def hook(self, event: str, *args):
+        pass  # TODO BaseClock hook()
--- sardine/clock/Time.py
+++ sardine/clock/Time.py
@@ -1,54 +1,19 @@
-import contextlib
-import contextvars
-
-time_shift = contextvars.ContextVar("time_shift", default=0.0)
-"""
-This specifies the amount of time to offset in the current context.
-Usually this is updated within the context of scheduled functions
-to simulate sleeping without actually blocking the function. Behavior is
-undefined if time is shifted in the global context.
-"""
-
 class Time:
     def __init__(
         self,
-        elapsed_time: float=0.0,
+        origin: float = 0.0,
     ):
-        self._elapsed_time = elapsed_time
+        self.origin = origin
 
     def __repr__(self) -> str:
-        return f"Started: {self._elapsed_time} seconds ago."
+        return "{}({})".format(
+            type(self).__name__,
+            " ".join(
+                f"{attr}={getattr(self, attr)}"
+                for attr in ("origin",)
+            ),
+        )
 
     def reset(self):
-        """Reset elasped time"""
-        self._elapsed_time = 0.0
-
-    @property
-    def elapsed_time(self) -> float:
-        """The amount of time elapsed including the current time shift."""
-        return self._elapsed_time + self.time_shift
-
-    @property
-    def time_shift(self) -> float:
-        """The time shift in the current context.
-
-        This is useful for simulating sleeps without blocking.
-        """
-        return time_shift.get()
-
-    @time_shift.setter
-    def time_shift(self, n_ticks: int):
-        time_shift.set(n_ticks)
-
-    @contextlib.contextmanager
-    def scoped_time_shift(self, seconds: float):
-        """Returns a context manager that adds `seconds` to the clock.
-
-        After the context manager is exited, the time shift is restored
-        to its previous value.
-        """
-        token = time_shift.set(time_shift.get() + seconds)
-        try:
-            yield
-        finally:
-            time_shift.reset(token)
+        """Resets the time origin back to 0."""
+        self.origin = 0.0

e11104291a69eecc30e6a66d7ae2483ab7e40e2e
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2022-11-17 16:16:13 -0500

    Initial sleep handler implementation

--- sardine/Handlers/SleepHandler.py
+++ sardine/Handlers/SleepHandler.py
@@ -1,16 +1,71 @@
-from typing import TYPE_CHECKING
-from ..base.handler import BaseHandler
+import asyncio
+from typing import Union
 
-if TYPE_CHECKING:
-    from ..fish_bowl import FishBowl
+from ..base import BaseHandler
+
 
 class SleepHandler(BaseHandler):
-    def __repr__(self) -> str:
-        return f"SleepHandler"
+    """The primary interface for other components to sleep."""
+    def __init__(self):
+        super().__init__()
+        self._interrupt_event = asyncio.Event()
+        self._wake_event = asyncio.Event()
+
+    # Public methods
+
+    async def _sleep(self, duration: Union[float, int]):
+        clock = self.env.clock
+        deadline = clock.time + duration
+        # TODO _sleep
+
+    async def sleep(self, duration: Union[float, int]):
+        # TODO sleep docstring
+        clock = self.env.clock
+        deadline = clock.time + duration
+        func = clock.sleep if clock.can_sleep() else self._sleep
+
+        while True:
+            await self._check_termination()
+            await self._wake_event.wait()
+
+            duration = deadline - clock.time
+            sleep_task = asyncio.create_task(func(duration))
+            intrp_task = asyncio.create_task(self._interrupt_event.wait())
+            tasks = (sleep_task, intrp_task)
+
+            try:
+                done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+            finally:
+                for t in tasks:
+                    t.cancel()
+
+            if sleep_task in done:
+                return
+
+    # Internal methods
+
+    async def _check_termination(self):
+        """Cancels the current task if the fish bowl is stopped."""
+        # This might be called after teardown, in which case `env` is None
+        if self.env is None or not self.env.is_running():
+            asyncio.current_task().cancel()
+
+    # Handler hooks
 
     def setup(self):
-        for event in ():  # TODO specify sleephandler events
+        for event in ("pause", "resume", "stop"):
             self.register(event)
 
+    def teardown(self):
+        self._interrupt_event.set()
+
     def hook(self, event: str, *args):
-        ...  # TODO sleephandler hook()
+        if event == "pause":
+            self._interrupt_event.set()
+            self._wake_event.clear()
+        elif event == "resume":
+            self._wake_event.set()
+            self._interrupt_event.clear()
+        elif event == "stop":
+            self._interrupt_event.set()
+            self._wake_event.set()  # just in case

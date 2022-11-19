e223a7d90ddf3d6ff6956f2c87a7f5ef1a934834
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2022-11-19 10:11:25 -0500

    Add optional sans-I/O event loop
    Depending on the system this might increase stability, given
    that Proactor vs Selector on windows had micro-instabilities

--- sardine/event_loop.py
+++ sardine/event_loop.py
@@ -1,5 +1,6 @@
 import asyncio
 import time
+from typing import Optional
 
 import rich
 
@@ -15,38 +16,75 @@ class PerfCounterMixin:
         return time.perf_counter()
 
 
+class SansSelector:
+    _event_list = []
+
+    def select(self, timeout: Optional[int]):
+        timeout = timeout or 0.0
+        time.sleep(timeout)
+        return self._event_list
+
+
+class SansIOEventLoop(asyncio.BaseEventLoop):
+    """An event loop implementation with zero I/O support.
+
+    This removes the potential overhead of waiting on I/O from selectors,
+    replacing it with `time.sleep()`. Any native I/O APIs will **not** work
+    when using this implementation.
+    """
+
+    _selector = SansSelector()
+
+    def _process_events(self, event_list):
+        pass
+
+# Precision mixins
+
 if hasattr(asyncio, "ProactorEventLoop"):
 
-    class PrecisionProactorEventLoop(
-        PerfCounterMixin, asyncio.ProactorEventLoop
-    ):
+    class PrecisionProactorEventLoop(PerfCounterMixin, asyncio.ProactorEventLoop):
         ...
 
 else:
     PrecisionProactorEventLoop = None
 
 
+class PrecisionSansIOEventLoop(PerfCounterMixin, SansIOEventLoop):
+    ...
+
+
 class PrecisionSelectorEventLoop(PerfCounterMixin, asyncio.SelectorEventLoop):
     ...
 
+# Policies
 
 class PrecisionProactorEventLoopPolicy(asyncio.DefaultEventLoopPolicy):
     _loop_factory = PrecisionProactorEventLoop
 
 
+class PrecisionSansIOEventLoopPolicy(asyncio.DefaultEventLoopPolicy):
+    _loop_factory = PrecisionSansIOEventLoop
+
+
 class PrecisionSelectorEventLoopPolicy(asyncio.DefaultEventLoopPolicy):
     _loop_factory = PrecisionSelectorEventLoop
 
+# Injectors
 
 def _inject_precision_proactor() -> bool:
     if PrecisionProactorEventLoop is None:
-        rich.print(
-            "[yellow]Skipping precision event loop on non-Windows system"
-        )
+        rich.print("[yellow]Skipping precision event loop on non-Windows system")
         return False
 
     asyncio.set_event_loop_policy(PrecisionProactorEventLoopPolicy())
     rich.print("[yellow]Injected precision proactor event loop")
+    return True
+
+
+def _inject_precision_sansio() -> bool:
+    asyncio.set_event_loop_policy(PrecisionSansIOEventLoopPolicy())
+    rich.print("[yellow]Injected precision Sans I/O event loop")
+    rich.print("[bold red]WARNING: event loop does not networking/subprocesses")
     return True
 
 
@@ -70,6 +108,7 @@ def _inject_uvloop() -> bool:
 
 def inject_policy():
     methods = (
+        # _inject_precision_sansio,
         _inject_uvloop,
         _inject_precision_proactor,
         _inject_precision_selector,

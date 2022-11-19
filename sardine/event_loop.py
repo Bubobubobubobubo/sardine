c90a2d97df5d9e1fef8c3a0a30f148ea4887ee2d
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2022-11-19 09:26:38 -0500

    Add missing _clock_resolution to PerfCounterMixin

--- sardine/event_loop.py
+++ sardine/event_loop.py
@@ -8,6 +8,10 @@ __all__ = ("inject_policy",)
 
 
 class PerfCounterMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._clock_resolution = time.get_clock_info("perf_counter").resolution
+
     def time(self) -> float:
         return time.perf_counter()
 

7fc44999265de2e38750a78544533397a92c4baa
Raphael Forment <raphael.forment@gmail.com>
2022-11-28 11:52:11 +0100

    Tabula rasa for the 'Player' class

--- sardine/surfing/player.py
+++ sardine/surfing/player.py
@@ -1,9 +1,27 @@
-from typing import Callable, Union
-from rich.panel import Panel
-from rich import print
+from typing import Union
+from dataclasses import dataclass
+from enum import Enum, auto
 
 __all__ = ("Player",)
 
+NUMBER = Union[float, int]
+
+class PlayerType(Enum):
+    INACTIVE    = auto()
+    FUNCTION    = auto()
+    SUPERDIRT   = auto()
+    MIDINOTE    = auto()
+    MIDICONTROL = auto()
+    OSC         = auto()
+
+@dataclass
+class PlayerInformation:
+    player_type: str
+    message: dict
+    iterator: int
+    divisor: int
+    rate: NUMBER
+    iteration_count: int
 
 class Player:
 
@@ -13,56 +31,9 @@ class Player:
     div, rate, etc...
     """
 
-    def __init__(self, name: str, content: Union[None, dict] = {}):
+    def __init__(self, 
+                 name: str, 
+                 info: PlayerInformation,
+    ):
         self._name = name
-        self._content = content
-
-    @classmethod
-    def run(cls, func: Callable):
-        """
-        The run method can be used to schedule any arbitrary function in rhythm just
-        like if it was a regular swimming function with a lone function call nested
-        inside it.
-        """
-        return {"type": "function", "func": func}
-
-    @classmethod
-    def play(cls, *args, **kwargs):
-        """
-        The play method is analog to using the 'D()' send method (SuperDirt).
-        """
-        return {"type": "sound", "args": args, "kwargs": kwargs}
-
-    @classmethod
-    def play_midi(cls, *args, **kwargs):
-        """
-        The play_midi method is analog to using the 'M()' send method (MIDIOut).
-        """
-        return {"type": "MIDI", "args": args, "kwargs": kwargs}
-
-    @classmethod
-    def play_control(cls, *args, **kwargs):
-        """
-        The play_control method is analog to using the 'CC()' send method (CCSender).
-        """
-        return {"type": "MIDI", "args": args, "kwargs": kwargs}
-
-    @classmethod
-    def play_osc(cls, *args, **kwargs):
-        """
-        #TODO: what to do of this?
-        """
-        return {"type": "OSC", "args": args, "kwargs": kwargs}
-
-    def __rshift__(self, method_result):
-        """
-        Entry point for allocating a task to a given Player. Possible tasks are 'play',
-        'play_midi', 'play_osc', 'play_control' or 'run'. These methods all correspond
-        to one possible simple operation to pattern and more could be added in the
-        future.
-        """
-        print(Panel.fit(f"[yellow][[red]{self._name}[/red] update!][/yellow]"))
-        self._content = method_result
-
-    def __repr__(self) -> str:
-        return f"[Player {self._name}]: {self._content}, div: {self._div}, rate: {self._rate}"
+        self._info = info

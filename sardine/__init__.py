e3425cb02b351cf7d65db090fef8c71b5c8b7db3
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2022-12-01 12:41:40 -0500

    Export the sequences package from sardine
    We intentionally don't want to star-import it because it adds a lot of
    functions, some of which override the built-ins (other than print)

--- sardine/__init__.py
+++ sardine/__init__.py
@@ -1,4 +1,4 @@
-from . import base, clock, event_loop, fish_bowl, handlers, scheduler
+from . import base, clock, event_loop, fish_bowl, handlers, scheduler, sequences
 from .base import *
 from .clock import *
 from .fish_bowl import *

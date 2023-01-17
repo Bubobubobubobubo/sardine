33124a49bd3165a94245cde8d57b42b26e4ff124
Yassin Siouda <yassin@siouda.com>
2023-01-17 19:04:47 +0100

    feat(client): simple navbar

--- sardine/client/src/components/Menubar.js
+++ sardine/client/src/components/Menubar.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import './Menubar.css';
 import { Button, IconBtn } from './buttons';
+import Logo from "../static/sardine_logo_single.png"
 
 class Menubar extends React.Component {
   data = {}
   render() {
   return (
     <nav>
-        <h2>Sardine</h2>
+        <img className='logo' src={Logo} alt="Sardine Logo" />
         <div className='btn-container'>
           <IconBtn icon="faPlay" onClick={() => console.log('Play')} />
           <IconBtn icon="faStop" onClick={() => console.log('Stop')} />
           <IconBtn icon="faFloppyDisk" onClick={() => console.log('Save')} />
           <IconBtn icon="faUserGroup" onClick={() => console.log('Share')} />
-          <Button text="Mode" onClick={this.props.editingModeFunction} />   
+          <Button text={this.props.editingMode + "Mode"} onClick={this.props.editingModeFunction} />   
           <a href="https://sardine.raphaelforment.fr" target="_blank">
             <Button text="Open Docs" />
           </a>
-
         </div>
     </nav>
     )

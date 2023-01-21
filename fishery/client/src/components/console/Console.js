b1b9ea35dfc7680bd9bef226006853a04ded1e4f
Jean-michel Pothelune <jean-michel@pothelune.fr>
2023-01-21 03:09:32 +0100

    Console White

--- fishery/client/src/components/console/Console.js
+++ fishery/client/src/components/console/Console.js
@@ -1,20 +1,21 @@
 import React from "react";
+import './Console.css'
 
 const Console = ({ logs }) => {
 
 
     return (
         <div className="console">
-        <div className="console-header">
-            <h3>Console</h3>
-        </div>
-        <div className="console-content">
-            <ul>
-                {logs.map((log) => {
-                    return <li>{log}</li>
-                })}
-            </ul>
-        </div>
+            <div className="console-header">
+                <h3>Console</h3>
+            </div>
+            <div className="console-content">
+                <ul>
+                    {logs.map((log) => {
+                        return <li>{log}</li>
+                    })}
+                </ul>
+            </div>
         </div>
     );
 };

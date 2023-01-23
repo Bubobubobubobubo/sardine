c165ecf8b15f6ea588fa04eaa4377524690ba3ef
Jean-michel Pothelune <jean-michel@pothelune.fr>
2023-01-23 09:12:07 +0100

    Allows static build

--- fishery/client/src/routes/+layout.ts
+++ fishery/client/src/routes/+layout.ts
@@ -1,4 +1,4 @@
 // Prevents the layout from being prerendered
 // It helps to avoid dom related errors on the client
 export const ssr = false;
-export const prerender = false;
+export const prerender = true;

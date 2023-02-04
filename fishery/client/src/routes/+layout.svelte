e254c2e1d0a149a30c23080cca6083ab4fd3485e
Raphael Forment <raphael.forment@gmail.com>
2023-02-04 13:57:16 +0100

    redo formatting of the layout file

--- fishery/client/src/routes/+layout.svelte
+++ fishery/client/src/routes/+layout.svelte
@@ -18,7 +18,8 @@
 	import { listen, onIdle } from 'svelte-idle';
 	import { default_buffer } from '$lib/text/DummyText';
     import { tutorialText } from '$lib/text/TutorialText';
-	let inputted_characters: number = 0;
+
+	let inputted_characters = 0;
 
 	/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 	Initialise a list of code buffers by fetching them from the server.  We are fetching files 
@@ -31,7 +32,7 @@
 	// This is the data structure we use for storing text throughout the editor. It is basically
 	// a dictionary mapping of the APPDIRS/buffers directory structure.
     interface Dictionary<T> { [Key: string]: T; }
-	let SARDINE_BUFFERS: Dictionary<String> = {};
+	let SARDINE_BUFFERS: Dictionary<string> = {};
 	
 	// Fetching from the local server to grab the content of the files.
 	let response = fetch('http://localhost:8000/text_files', {
@@ -39,7 +40,7 @@
 		method: 'GET',
 	})
 	.then(response => response.json())
-	.then((data: Object) => {
+	.then((data: object) => {
 		for (let [key, value] of Object.entries(data)) {
 			SARDINE_BUFFERS["["+key[0]+"]"] = value.toString();
 		};
@@ -50,8 +51,7 @@
 	 * of all the text buffers to the Flask server. They will be parsed to
 	 * text files and saved in the APPDIRS/buffers folder for later usage.
 	 */ 
-	function saveBuffers(buffers: Object) {
-		let buffersToSave = buffers;
+	function saveBuffers(buffers: object) {
 		fetch("http://localhost:8000/save", {
 				credentials: 'include',
 				method: "POST", 
@@ -71,6 +71,8 @@
 	/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
 	// Initialise state of the code editor: we don't know anything about the state of anything.
 	// We don't really know what we are dealing with and the state is crazy complex. Fun ahead :)
+	/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+
 	let logs: string[] = [];
 	let extensions: any[] = [];
 	$: extensions = extensions;
@@ -82,135 +84,129 @@
 		runnerService.watchLogs((log) => {
 			logs = [...logs, log];
 		})
-	})
-
-    onMount(() => {
-      editorMode.subscribe(value => {
-        if (value == 'vim') {
-            console.log('Switch to VIM Mode.')
-            tick().then(() => {
-                view.addVim();
-            });
-        } else {
-            console.log('Switch to Emacs Mode.')
-            tick().then(() => {
-                view.removeVim();
-            });
-        }
-    });
-    });
-
-	/**
-	 * Intercepting keypresses and triggering action. The current events are covered:
-	 * - Editing Mode Change : pressing Ctrl + Space will switch between Vim and Emacs.
-	 * - Submitting code : pressing Shift + Enter or Ctrl + E will trigger code eval.
-	 * @param event Keypress or combination of multiple keys
-	 */
-	function keyDownHandler(event: KeyboardEvent): void {
-
-		// Cancel 'tab' from being used as a navigation key
-		if (event.key === "Tab") {
-			event.preventDefault();
-		}
-
-    	// Shift + Enter or Ctrl + E (Rémi Georges mode)
-		if(event.key === 'Enter' && event.shiftKey || event.key === 'e' && event.ctrlKey) {
-      		event.preventDefault(); // Prevents the addition of a new line
-			const code = view.getSelectedLines();
-			runnerService.executeCode(code + "\n\n");
-		}
-
-		// Keybinding to switch from Emacs mode to Vim Mode
-		if(event.key === ' ' && event.ctrlKey) {
-			event.preventDefault(); // Prevents the addition of a newline.
-			editorMode.update(n => n === 'emacs' ? 'vim' : 'emacs');
-		}
-
-		// TODO: implement animation whenever the user evaluates code
-		// TODO: implement switching tabs from keybindings
- 	}
-
-	/*
-	 * Some events are hard to catch on key up but can be prevented by chasing
-	 * key up events!
-	 */
-  	function keyUpHandler(event: KeyboardEvent): void {
-		// Prevent 'Esc' from defocusing the page (hard to catch)
-		if (event.key === "Escape" || event.keyCode === 27) {
-			event.preventDefault();
-		}
-	};
-
-	/**
-	 * Reacting to the play button by resuming the FishBowl.
-	 */
-	function handlePlay() {
-		runnerService.executeCode("bowl.dispatch('resume')")
-	}
-
-	/**
-	 * Reacting to the pause button by 'pausing' the FishBowl.
-	 */
-	function handleStop() {
-		runnerService.executeCode("bowl.dispatch('pause')")
-	}
-
-	function handleBufferChange({ detail: {tr} }) {
-		// Don't ask me what a tr is, no idea...
-
-		// Getting the currently active tab through introspection
-		let tab = get(activeTab);
-
-		// We ignore the scratch buffer! 
-		if (tab !== 0) {
-			// Writing the content of the buffer to the internal dict.
-			SARDINE_BUFFERS["["+(tab-1)+"]"] = tr._doc.text.join('\n');
-		}
-		
-		// Everytime the user enters more than 50 characters, save the files to the disk!
-		inputted_characters += 1;
-		if (inputted_characters % 50 == 0) {
-			saveBuffers(SARDINE_BUFFERS);
-		}
-	}
-
-    function saveAsTextFile() {
-        // Querying the content of the buffer
-        let file = new Blob(
-            [tr._doc.text.join('\n')],
-            { type: "text/plain;charset=utf-8" },
-        );
-        FileSaver.saveAs(file, "sardine.py");
-    }
-
-    function spawnTutorial() {
-        console.log('Spawning the basic tutorial');
-        // We change the buffer but we need to trigger a redraw as well
-        SARDINE_BUFFERS["[*]"] = tutorialText;
-        view._setText(tutorialText);
-    }
-
-    function openSardineFolder() {
-        // Will ask Flask to open the Sardine default folder
-        console.log("Opening Sardine Folder");
-        const response = fetch('http://localhost:8000/open_folder', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: {} 
+        editorMode.subscribe(value => {
+            if (value == 'vim') {
+                console.log('Switch to VIM Mode.')
+                tick().then(() => {
+                    view.addVim();
+                });
+            } else {
+                console.log('Switch to Emacs Mode.')
+                tick().then(() => {
+                    view.removeVim();
+                });
+            }
         });
-    }
+    });
 
-	// This will trigger a save rather frequently. This value needs some finetuning
-	// to be less aggressive! I wonder what effect it can have on performances.
-	listen({
-		timer: 10,
-		cycle: 500
-	}); 
-	onIdle(() => {
-		saveBuffers(SARDINE_BUFFERS);
-	});
+ /**
+  * Intercepting keypresses and triggering action. The current events are covered:
+  * - Editing Mode Change : pressing Ctrl + Space will switch between Vim and Emacs.
+  * - Submitting code : pressing Shift + Enter or Ctrl + E will trigger code eval.
+  * @param event Keypress or combination of multiple keys
+  */
+  function keyDownHandler(event: KeyboardEvent): void {
+
+      // Cancel 'tab' from being used as a navigation key
+      if (event.key === "Tab") {
+          event.preventDefault();
+      }
+
+      // Shift + Enter or Ctrl + E (Rémi Georges mode)
+      if(event.key === 'Enter' && event.shiftKey || event.key === 'e' && event.ctrlKey) {
+          event.preventDefault(); // Prevents the addition of a new line
+          const code = view.getSelectedLines();
+          runnerService.executeCode(code + "\n\n");
+      }
+
+      // Keybinding to switch from Emacs mode to Vim Mode
+      if(event.key === ' ' && event.ctrlKey) {
+          event.preventDefault(); // Prevents the addition of a newline.
+              editorMode.update(n => n === 'emacs' ? 'vim' : 'emacs');
+      }
+  }
+
+  /*
+  * Some events are hard to catch on key up but can be prevented by chasing
+  * key up events!
+  */
+
+  function keyUpHandler(event: KeyboardEvent): void {
+      // Prevent 'Esc' from defocusing the page (hard to catch)
+      if (event.key === "Escape" || event.keyCode === 27) {
+          event.preventDefault();
+      }
+  };
+
+  /**
+  * Reacting to the play button by resuming the FishBowl.
+  */
+  function handlePlay() {
+      runnerService.executeCode("bowl.dispatch('resume')")
+  }
+
+  /**
+   * Reacting to the pause button by 'pausing' the FishBowl.
+   */
+
+  function handleStop() {
+      runnerService.executeCode("bowl.dispatch('pause')")
+  }
+
+  function handleBufferChange({ detail: {tr} }) {
+      // Getting the currently active tab through introspection
+      let tab = get(activeTab);
+
+      // We ignore the scratch buffer!
+      if (tab !== 0) {
+          // Writing the content of the buffer to the internal dict.
+              SARDINE_BUFFERS["["+(tab-1)+"]"] = tr._doc.text.join('\n');
+      }
+
+      // Everytime the user enters more than 50 characters, save the files to the disk!
+      inputted_characters += 1;
+      if (inputted_characters % 50 == 0) {
+          saveBuffers(SARDINE_BUFFERS);
+      }
+  }
+
+  function saveAsTextFile() {
+      // Querying the content of the buffer
+      let file = new Blob(
+          [tr._doc.text.join('\n')],
+          { type: "text/plain;charset=utf-8" },
+      );
+      FileSaver.saveAs(file, "sardine.py");
+  }
+
+  function spawnTutorial() {
+      console.log('Spawning the basic tutorial');
+      // We change the buffer but we need to trigger a redraw as well
+      SARDINE_BUFFERS["[*]"] = tutorialText;
+      view._setText(tutorialText);
+  }
+
+  function openSardineFolder() {
+      // Will ask Flask to open the Sardine default folder
+      console.log("Opening Sardine Folder");
+      const response = fetch('http://localhost:8000/open_folder', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: {}
+      });
+  }
+
+  // This will trigger a save rather frequently. This value needs some finetuning
+  // to be less aggressive! I wonder what effect it can have on performances.
+  listen({
+      timer: 10,
+      cycle: 500
+  });
+  onIdle(() => {
+      saveBuffers(SARDINE_BUFFERS);
+  });
 
 
 </script>

13a2c0093b4edec5be82b1bc812c30282e414e85
Raphael Forment <raphael.forment@gmail.com>
2023-02-16 15:28:31 +0100

    Fixing bug with creating files at init

--- fishery/server/flask_server.py
+++ fishery/server/flask_server.py
@@ -19,6 +19,11 @@ logging.getLogger("werkzeug").disabled = True
 APP_NAME, APP_AUTHOR = "Sardine", "Bubobubobubo"
 USER_DIR = Path(user_data_dir(APP_NAME, APP_AUTHOR))
 LOG_FILE = USER_DIR / "sardine.log"
+FILENAMES = [
+        "0.py", "1.py", "2.py", "3.py",
+        "4.py", "5.py", "6.py", "7.py",
+        "8.py", "9.py", "*.py",
+]
 
 # We need to create the log file if it doesn't already exist
 Path(LOG_FILE).touch(exist_ok=True)
@@ -38,12 +43,24 @@ class WebServer():
         self.host, self.port = host, port
         self.local_files = self.load_buffer_files()
 
+
+    def check_buffer_files(self) -> None:
+        """This function will check the integrity of the buffer folder.""" 
+        buffer_folder = Path(USER_DIR / "buffers")
+        for filename in FILENAMES:
+            check_file: Path = buffer_folder / filename
+            if not check_file.exists():
+                print(f'Creating file {str(filename)}!')
+                Path(check_file).touch()
+            else:
+                print(f'Loading file {str(filename)}')
+
+
     def load_buffer_files(self) -> Optional[dict]:
         """
-        Loading buffer files from a local folder. If the folder doesn't 
-        exis, this function will automatically create it and load empty 
-        files for the first round. If the folder exists, read files in their 
-        current state
+        Loading buffer files from a local folder. If the folder doesn't exist, this 
+        function will automatically create it and load empty files for the first round. 
+        If the folder exists, read files in their current state
         """
         buffer_files: dict = {}
 
@@ -51,7 +68,7 @@ class WebServer():
         if not os.path.isdir(str(USER_DIR / "buffers")):
             try:
                 os.makedirs(str(USER_DIR / "buffers"))
-                for filename in range(0, 10):
+                for filename in list(range(0, 10)):
                     print(f"Creating file {filename}.py.")
                     Path(USER_DIR / "buffers" / f"{filename}.py").touch()
                     buffer_files[filename] = ""
@@ -61,6 +78,7 @@ class WebServer():
                 exit()
         # If it already exists, read files from the folder
         else:
+            self.check_buffer_files()
             buffer_folder = Path(USER_DIR / "buffers")
             for file in os.listdir(buffer_folder):
                 with open(buffer_folder / file, 'r') as buffer:

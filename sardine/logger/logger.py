43ffefa2fab49e357fe60e86c7e52e7868ebbe09
johnchem <j.payssan@gmail.com>
2023-03-30 11:33:45 +0200

    clean the logger file

--- sardine/logger/logger.py
+++ sardine/logger/logger.py
@@ -13,21 +13,9 @@ APP_NAME, APP_AUTHOR = "Sardine", "Bubobubobubo"
 USER_DIR = Path(user_data_dir(APP_NAME, APP_AUTHOR))
 LOG_FILE = USER_DIR / "sardine.log"
 
-# Logging file for testing
-TEST_FILE = USER_DIR / "test.log"
-
-# The file needs to exist to actually log something
-if not LOG_FILE.exists():
-    LOG_FILE.touch()
-
-# Actual logging logic
-report_file = open(LOG_FILE, "wt", encoding="utf-8")
-file_console = Console(file=report_file)
-
 terminal_console = Console()
 
 def print(*args, **kwargs):
-    file_console.print(*args, **kwargs)
     terminal_console.print(*args, **kwargs)
 
 CONFIG = '''
@@ -76,5 +64,5 @@ CONFIG = '''
 
 # load the JSON config and overwrite the filename variable to accept a variable path
 config = json.loads(CONFIG)
-config["handlers"]["file_handler"]["filename"] = TEST_FILE
+config["handlers"]["file_handler"]["filename"] = LOG_FILE
 logging.config.dictConfig(config)

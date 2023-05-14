5d443245da4ec3990e266e9172868f31f00a8f78
Raphael Forment <raphael.forment@gmail.com>
2023-05-14 14:16:36 +0200

    Remove notify_tidal_streams from clock
    This function should be run from the asyncrunner in charge of querying
    patterns. Better synchronisation, more consistant timing, etc...

--- sardine/clock/link_clock.py
+++ sardine/clock/link_clock.py
@@ -74,40 +74,6 @@ class LinkClock(BaseThreadedLoopMixin, BaseClock):
         """Equivalent to Ableton Link timeAtBeat method"""
         return self.internal_origin + (self.beat / self.bps)
 
-    # def _notify_tidal_streams(self):
-    #     """
-    #     Notify Tidal Streams of the current passage of time.
-    #     """
-    #     self.tick += 1
-
-    #     # Logical time since the clock started ticking: sum of frames
-    #     logical_now, logical_next = (
-    #         self.internal_origin + (self.tick * self._framerate),
-    #         self.internal_origin + ((self.tick + 1) * self._framerate),
-    #     )
-
-    #     # Current time (needed for knowing wall clock time)
-    #     now = self.shifted_time + self._tidal_nudge
-
-    #     # Wall clock time for the "ideal" logical time
-    #     cycle_from, cycle_to = (
-    #         self.beatAtTime(logical_now) / (self.beats_per_bar * 2),
-    #         self.beatAtTime(logical_next) / (self.beats_per_bar * 2),
-    #     )
-
-    #     # Sending to each individual subscriber for scheduling using timestamps
-    #     try:
-    #         for sub in self.env._vortex_subscribers:
-    #             sub.notify_tick(
-    #                 clock=self,
-    #                 cycle=(cycle_from, cycle_to),
-    #                 cycles_per_second=self.cps,
-    #                 beats_per_cycle=(self.beats_per_bar * 2),
-    #                 now=now,
-    #             )
-    #     except Exception as e:
-    #         print(e)
-
     ## GETTERS  ################################################
 
     @property

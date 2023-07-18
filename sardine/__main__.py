661b1e26108e7862722ba3da4481c830bd66e72f
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2023-07-18 12:29:19 -0400

    Remove web editor from sardine

--- sardine/__main__.py
+++ sardine/__main__.py
@@ -65,50 +65,6 @@ def main(ctx: click.Context):
         console.start()
 
 
-# fishery web
-# fishery web --host
-# fishery web --port
-# fishery web --host --port
-# fishery config
-@main.command(
-    short_help="Starts sardine as a web server",
-    help="""
-        This command starts sardine as a web server. The server can be accessed
-        at http://localhost:8000 by default.
-        """,
-)
-@click.option(
-    "-h",
-    "--host",
-    default="localhost",
-    help="The host to bind the server to.",
-    show_default=True,
-    type=str,
-)
-@click.option(
-    "-p",
-    "--port",
-    default=8000,
-    help="The port to bind the server to.",
-    show_default=True,
-    type=int,
-)
-@click.option(
-    "--no-browser",
-    is_flag=True,
-    help="Prevents the server from opening a browser window.",
-)
-def web(host: str, port: int, no_browser: bool):
-    from .server import WebServer
-
-    consoleManager = ConsoleManager()
-    server = WebServer(host=host, port=port)
-    server.start_in_thread(consoleManager.console)
-    if not no_browser:
-        server.open_in_browser()
-    consoleManager.start()
-
-
 @main.command(
     short_help="Starts sardine configuration tool",
     help="This command starts Sardine configuration tool.",
